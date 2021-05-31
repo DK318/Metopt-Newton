6ab1548e30e50dfd37eb823a2355fe77c3231cc9
ArtemShashulovskiy <stratosphere665@gmail.com>
2021-06-01 00:00:10 +0300

    Expression interface refactoring

--- src/expression/Expression.java
+++ src/expression/Expression.java
@@ -1,7 +1,12 @@
 package expression;
 
+import java.util.Set;
+
 public interface Expression {
     double evaluate(double... args);
     Expression differentiate(int var);
-    int getArity();
+    Set<Integer> getArguments();
+    default int getArity() {
+        return getArguments().size();
+    }
 }
