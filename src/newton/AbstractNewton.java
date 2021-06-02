f008d65f136f517e8921d8cc010a952bf2a527d2
ArtemShashulovskiy <stratosphere665@gmail.com>
2021-06-02 23:37:19 +0300

    Tabulator

--- src/newton/AbstractNewton.java
+++ src/newton/AbstractNewton.java
@@ -5,6 +5,9 @@ import matrix.Gradient;
 import matrix.HesseMatrix;
 import util.MatrixUtil;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class AbstractNewton {
     protected final Expression function;
     protected final Gradient gradient;
@@ -13,7 +16,7 @@ public abstract class AbstractNewton {
     protected double[] p;
     protected final double eps;
 
-    public AbstractNewton(Expression function, double[] x, double eps) {
+    public AbstractNewton(final Expression function, final double[] x, final double eps) {
         this.function = function;
         this.gradient = new Gradient(function);
         this.hesseMatrix = new HesseMatrix(function);
@@ -26,22 +29,59 @@ public abstract class AbstractNewton {
     protected abstract double getAlpha();
 
     public double[] minimize() {
+        final List<Iteration> iteratios = getIteratios();
+        return iteratios.get(iteratios.size() - 1).x;
+    }
+
+    public List<Iteration> getIteratios() {
+        final List<Iteration> iterations = new ArrayList<>();
         while (true) {
-            double[] minusGrad = MatrixUtil.multiplyByScalar(gradient.evaluate(x), -1);
+            final double[] minusGrad = MatrixUtil.multiplyByScalar(gradient.evaluate(x), -1);
             if (halt(minusGrad)) {
                 break;
             }
-            double[][] hesse = hesseMatrix.evaluate(x);
+            final double[][] hesse = hesseMatrix.evaluate(x);
             p = getDirection(hesse, minusGrad);
 
-            double alpha = getAlpha();
+            final double alpha = getAlpha();
 
             x = MatrixUtil.add(x, MatrixUtil.multiplyByScalar(p, alpha));
+            iterations.add(new Iteration(iterations.size() + 1, MatrixUtil.norm(p), x, alpha));
         }
-        return x;
+        return iterations;
     }
 
-    private boolean halt(double[] p) {
+    private boolean halt(final double[] p) {
         return MatrixUtil.norm(p) < eps;
+    }
+
+    public static class Iteration {
+        private int n;
+        private double p;
+        private double[] x;
+        private double alpha;
+
+        public Iteration(final int n, final double p, final double[] x, final double alpha) {
+            this.n = n;
+            this.p = p;
+            this.x = x;
+            this.alpha = alpha;
+        }
+
+        public int getN() {
+            return n;
+        }
+
+        public double getP() {
+            return p;
+        }
+
+        public double[] getX() {
+            return x;
+        }
+
+        public double getAlpha() {
+            return alpha;
+        }
     }
 }
---
+++ src/util/tabulator/Tabulator.java
@@ -0,0 +1,54 @@
+package util.tabulator;
+
+import expression.*;
+import newton.AbstractNewton;
+import newton.ClassicalNewton;
+import newton.DescendNewton;
+import newton.OneDimensionalNewton;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Tabulator {
+
+    private Tabulator() {
+        // Utility class
+    }
+
+    public static void tabulate(final Expression function, final double eps, final double... x) {
+        tabulate(new ClassicalNewton(function, x, eps));
+        tabulate(new OneDimensionalNewton(function, x, eps));
+        tabulate(new DescendNewton(function, x, eps));
+    }
+
+    private static void tabulate(final AbstractNewton newtonMethod) {
+        final List<AbstractNewton.Iteration> iteratios = newtonMethod.getIteratios();
+
+        System.out.println("Newton method: " + newtonMethod.getClass().getSimpleName());
+        System.out.println("\\begin{tabular}{|c|c|c|c|}");
+        System.out.println("\t \\hline Итерация & $|p_k|$ & $x_k$ & $\\alpha$ \\\\");
+        iteratios.forEach(Tabulator::printIteration);
+        System.out.println("\\end{tabular}");
+    }
+
+    private static void printIteration(final AbstractNewton.Iteration iteration) {
+        System.out.printf("\t \\hline $%s$ & $%s$ & $%s$ & $%s$ \\\\%n",
+                iteration.getN(),
+                iteration.getP(),
+                getX(iteration.getX()),
+                iteration.getAlpha());
+    }
+
+    private static String getX(final double[] x) {
+        return String.format("(%s)", Arrays.stream(x).mapToObj(Double::toString).collect(Collectors.joining(", ")));
+    }
+
+    public static void main(final String[] args) {
+        tabulate(new Add(
+                new Square(new Subtract(new Variable(1), 3)),
+                new Square(new Subtract(new Variable(2), 4))
+        ), 1e-2, 0, 0);
+    }
+}
